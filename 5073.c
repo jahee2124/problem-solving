7306d2cf4a008de3ceb84a1f7711ddd6e2ec4ac7
jahee2124 <jahee2124@gmail.com>
2026-01-06 18:49:32 +0900

    boj 5073

--- 5073.c
+++ 5073.c
@@ -8,15 +8,15 @@ int main(){
         scanf("%d %d %d", &a, &b, &c);
         if (a == 0 && b == 0 && c == 0) break;
         else if ((a + b) <= c || (b + c) <= a || (c + a) <= b){
-            printf("Invalid");
+            printf("Invalid\n");
             continue;
         }
         else if (a == b || b == c || c == a){
-            if (a == b && b == c) printf("Equuilateral");
-            else printf("Isosceles");
+            if (a == b && b == c) printf("Equilateral\n");
+            else printf("Isosceles\n");
             continue;
         }
-        else printf("Scalene");
+        else printf("Scalene\n");
     }
     return 0;
 }
